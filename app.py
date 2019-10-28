51cd9383fa307244e7507cea2bc12779242d6b15
Maxim Burgerhout <maxim@redhat.com>
2019-10-28 10:59:49 +0100

    database setup

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import json
 import os
+import psycopg2
 from kafka import KafkaConsumer
+
+print('Setting up database...')
+conn_string = "host={}, database={}, user={}, password={}".format(os.environ['DB_HOSTNAME'], os.environ['DB_NAME'], os.environ['DB_USER'], os.environ['DB_PASSWORD'])
+conn = psycopg2.connect(conn_string)
+create_table_sql = """create table if not exists twitter (
+    id serial primary key,
+    name varchar(255) not null,
+    screenname varchar(255) not null,
+    userid integer not null,
+    url text not null
+    )
+    """
+cur = conn.cursor()
+cur.execute(create_table_sql)
 
 print('Running Consumer...')
 
@@ -14,9 +29,10 @@ consumer = KafkaConsumer(topic_name, group_id=consumer_group,
 
 for msg in consumer:
     txt = json.loads(msg.value)
-    print("name: " + txt['twitterName'])
-    print("screenname: " + txt['twitterScreenName'])
-    print("id: " + str(txt['twitterID']))
-    print("timestamp: " + txt['tweetCreatedAt'])
-    for url in txt['url']:
-        print("url: " + url)
+    if os.environ['DEBUG']:
+        print("name: " + txt['twitterName'])
+        print("screenname: " + txt['twitterScreenName'])
+        print("id: " + str(txt['twitterID']))
+        print("timestamp: " + txt['tweetCreatedAt'])
+        for url in txt['url']:
+            print("url: " + url)
