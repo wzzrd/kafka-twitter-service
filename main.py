cc12d1323464e1f638996fc49ec88da59264a350
Maxim Burgerhout <maxim@redhat.com>
2019-10-28 08:49:48 +0100

    Making variables

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from kafka import KafkaConsumer
 print('Running Consumer...')
 
 parsed_records = []
-# topic_name = 'twitter-stuff'
-topic_name = 'another-topic'
+topic_name = os.environ['KAFKA_TOPIC']
+bootstrap_servers = os.environ['KAFKA_BOOTSTRAP_SERVER'].split(",")
+consumer_group = os.environ['KAFKA_CONSUMER_GROUP']
 
-consumer = KafkaConsumer(topic_name, group_id='pythongroup',
-                         bootstrap_servers=['172.30.206.199:9092'], api_version=(0, 10))
+consumer = KafkaConsumer(topic_name, group_id=consumer_group,
+                         bootstrap_servers=bootstrap_servers, api_version=(0, 10))
 
 for msg in consumer:
     txt = msg.value
