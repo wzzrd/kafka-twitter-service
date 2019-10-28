211854fef5ea92d2fe8ae9bd7782660118745350
Maxim Burgerhout <maxim@redhat.com>
2019-10-28 10:08:34 +0100

    Disable offset commits for now

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ topic_name = os.environ['KAFKA_TOPIC']
 bootstrap_servers = os.environ['KAFKA_BOOTSTRAP_SERVER'].split(",")
 consumer_group = os.environ['KAFKA_CONSUMER_GROUP']
 
-consumer = KafkaConsumer(topic_name, group_id=consumer_group,
+consumer = KafkaConsumer(topic_name, 
                          bootstrap_servers=bootstrap_servers, api_version=(0, 10))
 
 for msg in consumer:
