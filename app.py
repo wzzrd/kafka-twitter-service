0d7b534d674d731a368b9a7530db37e111d16277
Maxim Burgerhout <maxim@redhat.com>
2019-10-28 12:48:43 +0100

    24 hours

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ for msg in consumer:
         '{}',
         '{}',
         '{}',
-        to_timestamp('{}', 'YYYY-MM-DD HH:MI:SS'),
+        to_timestamp('{}', 'YYYY-MM-DD HH24:MI:SS'),
         '{}'
         ) on conflict do nothing
         """.format(txt['twitterName'], 
